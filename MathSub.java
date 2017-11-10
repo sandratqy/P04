a98c10eca622779177797fbf8cf131b42d3cd1c0
161189K SANDRA TAN QIU YI <161189K@sit.nyp.edu.sg>
2017-11-10 10:10:14 +0800

    Add MathSub

--- MathSub.java
+++ MathSub.java
@@ -3,9 +3,9 @@ public class MathAdd {
 	public MathAdd {}
 	
 	static public int add(int a, int b) {   
-        return a + b;
+        return a - b;
 	
-	system.out.println(a + b);
+	system.out.println(a - b);
 	}
 	
 }
